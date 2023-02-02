8b080b4dcddfafe11947f7f6bb982fde3c9e4219
hollan2 <hollan2@pdx.edu>
2023-02-01 22:49:11 -0800

    Create github practice exercise

---
+++ Github Exercise/exercise.py
@@ -0,0 +1,13 @@
+# Welcome to the github practice exercise! 
+# Please edit the *silent* next to your name with your own message and push it up to our github on a new branch!
+
+print(" Github Exercise!\n-------------------")
+print("Roll Call!\n")
+print("Adam - *silent*")
+print("Ben - *silent*")
+print("Caleb - *silent*")
+print("Chloe - *silent*")
+print("DJ - *silent*")
+print("Naton - *silent*")
+print("Viktoriya - *silent*")
+print("Vinh - *silent*")
