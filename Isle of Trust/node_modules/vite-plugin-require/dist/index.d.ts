f4ecbb6826b2241d47be4ea2812f3160c50bb9f8
hollan2 <hollan2@pdx.edu>
2023-02-25 17:01:37 -0800

    I don't wanna talk about it part 2

--- Isle of Trust/node_modules/vite-plugin-require/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Plugin } from "vite";
-export default function vitePluginRequire(opts?: {
-    fileRegex?: RegExp;
-    log?: (...arg: any[]) => void;
-    translateType?: "importMetaUrl" | "import";
-}): Plugin;
