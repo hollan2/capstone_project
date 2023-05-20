5a104b21146654874f9fa0c7d615da5f17970241
Adam Stagner <stagner2@pdx.edu>
2023-05-19 18:30:55 -0700

    Susi is now a student

--- Isle of Trust/src/models/agent.ts
+++ Isle of Trust/src/models/agent.ts
@@ -412,7 +412,7 @@ export class Ideology extends AttributeContainer {
         }
 
         if (generosity == 19){ this.role = 4;}
-        else if (generosity == 15){ this.role = 1;}
+        else if (generosity == 15){ this.role = 2;}
         else if (generosity == 10){ this.role = 3;}
         else if (generosity == 5){ this.role = 1;}
         else { this.role = Math.floor(Math.random() * 6);}
