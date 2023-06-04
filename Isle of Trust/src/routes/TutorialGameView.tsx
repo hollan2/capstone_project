7222e7c2d485fe875d858a17ecc24670c2dea5ad
Naton Cai <ncai@pdx.edu>
2023-06-04 15:27:31 -0700

    fixed problem with the stats

--- Isle of Trust/src/routes/TutorialGameView.tsx
+++ Isle of Trust/src/routes/TutorialGameView.tsx
@@ -263,7 +263,9 @@ class TutorialView extends React.Component<StartInfo, GameViewState> {
                 tally = new choiceTally();
                 tally.tallyChoices(relation.history);
                 sumChoices.together += tally.together;
+                sumChoices.solo += tally.solo;
                 sumChoices.cheated += tally.cheated;
+                sumChoices.honest += tally.honest;
             });
         }
         return sumChoices;
